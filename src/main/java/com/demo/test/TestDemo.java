2dff8cbd66c2f62668a80f05bf5083cff82d9959
lin <1159647024@qq.com>
2018-06-04 15:49:05 +0800

    修改了TestDemo 新增了TestDemo2

--- src/main/java/com/demo/test/TestDemo.java
+++ src/main/java/com/demo/test/TestDemo.java
@@ -4,5 +4,6 @@ public class TestDemo {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("你好");
+        System.out.println("测试github");
     }
 }
