10e9476978dc6e769f5de07856459c92d4ba3298
lin <1159647024@qq.com>
2018-06-04 15:33:18 +0800

    idea连接GitHub测试

---
+++ src/main/java/com/demo/test/TestDemo.java
@@ -0,0 +1,8 @@
+package com.demo.test;
+
+public class TestDemo {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("你好");
+    }
+}
