7f3e616197d5d51718d6fdddd5e8efa0e8a66413
lin8023yaya <38373091+lin8023yaya@users.noreply.github.com>
2018-06-08 13:21:54 +0800

    Update TestDemo.java
    测试冲突解决

--- src/main/java/com/demo/test/TestDemo.java
+++ src/main/java/com/demo/test/TestDemo.java
@@ -3,7 +3,7 @@ package com.demo.test;
 public class TestDemo {
     public static void main(String[] args){
         System.out.println("hello world");
-        System.out.println("你好");
+        System.out.println("你好不好");
         System.out.println("测试github");
     }
 }
