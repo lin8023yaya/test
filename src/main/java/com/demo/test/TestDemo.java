c53895dd6f414fa70d0216637451a8a6747ac029
lin <1159647024@qq.com>
2018-06-08 13:33:07 +0800

    修改了TestDemo 增加一句话来测试冲突

--- src/main/java/com/demo/test/TestDemo.java
+++ src/main/java/com/demo/test/TestDemo.java
@@ -5,5 +5,6 @@ public class TestDemo {
         System.out.println("hello world");
         System.out.println("你好");
         System.out.println("测试github");
+        System.out.println("只有自己是不是更加应该去努力搞点事情!");
     }
 }
--- src/main/java/com/demo/test/TestDemo2.java
+++ src/main/java/com/demo/test/TestDemo2.java
@@ -2,6 +2,6 @@ package com.demo.test;
 
 public class TestDemo2 {
     public static void main(String[] args){
-        System.out.println("only to test");  
+        System.out.println("only to test");
     }  
 }
